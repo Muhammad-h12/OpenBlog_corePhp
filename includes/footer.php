43bfe0a0682b5956615654542c2f1769d7b9eff3
Ridha <m.haroonw12@gmail.com>
2023-11-02 22:50:51 +0530

    made navbar dynamic according to category, also in homepage

---
+++ includes/footer.php
@@ -0,0 +1,44 @@
+<!-- ====== Footer Section Start -->
+<footer class="wow fadeInUp relative z-10 bg-black pt-2 lg:pt-[10px]" ">
+<div class="mt-12 border-t border-opacity-40 py-8 lg:mt-[60px]">
+    <div class="container">
+        <div class="-mx-4 flex flex-wrap">
+            <div class="w-full px-4 md:w-2/3 lg:w-1/2">
+
+            </div>
+            <div class="w-full px-4 md:w-1/3 lg:w-1/2">
+                <div class="my-1 flex justify-center md:justify-end">
+                    <p class="text-base text-[#f3f4fe]">
+                        Designed and Developed by
+                        <a
+                            href="https://tailgrids.com"
+                            rel="nofollow noopner"
+                            target="_blank"
+                            class="text-primary hover:underline"
+                        >
+                            TailGrids and UIdeck
+                        </a>
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</footer>
+
+
+<!-- ====== Back To Top Start -->
+<a
+    href="javascript:void(0)"
+    class="back-to-top fixed bottom-8 right-8 left-auto z-[999] hidden h-10 w-10 items-center justify-center rounded-md bg-primary text-white shadow-md transition duration-300 ease-in-out hover:bg-dark"
+>
+      <span
+          class="mt-[6px] h-3 w-3 rotate-45 border-t border-l border-white"
+      ></span>
+</a>
+<!-- ====== Back To Top End -->
+
+<!-- ====== All Scripts -->
+<script src="assets/js/main.js"></script>
+</body>
+</html>
---
+++ includes/header.php
@@ -0,0 +1,29 @@
+<?php
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>CMS Home | Core PHP</title>
+    <link
+            rel="shortcut icon"
+            href="assets/images/favicon.png"
+            type="image/x-icon"
+                />
+    <link rel="stylesheet" href="assets/css/animate.css" />
+    <link rel="stylesheet" href="assets/css/tailwind.css" />
+
+    <!-- ==== WOW JS ==== -->
+    <script src="assets/js/wow.min.js"></script>
+    <script>
+new WOW().init();
+    </script>
+</head>
+<body>
+
+<!--// include Navbar-->
+
+<?php include_once "includes/navbar.php";?>
